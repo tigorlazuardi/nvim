218fbe5640d48a75339dda84603508a04401a01e
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-08-12 16:16:24 +0700

    nvim: removed mason path override

--- lua/plugins/mason.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "mason.nvim",
-    opts = {
-        -- NixOS packages should override Mason packages if exist
-        PATH = vim.loop.os_uname().version:find("NixOS") and "append" or "prepend",
-    },
-}
