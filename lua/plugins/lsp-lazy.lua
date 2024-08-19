3fd8bf10705d7c0f2e5e2cef5876a346fed2955c
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-08-19 08:44:47 +0700

    lsp-lazy: exclude postgres_lsp by default

--- lua/plugins/lsp-lazy.lua
+++ lua/plugins/lsp-lazy.lua
@@ -5,6 +5,7 @@ return {
         prefer_local = true,
         excluded_servers = {
             "gopls", -- gopls likes to be double attached if enabled here.
+            "postgres_lsp",
         },
         preferred_servers = {
             nix = {
