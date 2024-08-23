f1b856215b5b7e8432f42c98d02e6223bb5cfce9
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-08-23 07:54:17 +0700

    copilot: add CopilotChat.nvim

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -25,4 +25,18 @@ return {
         "zbirenbaum/copilot-cmp",
         enabled = false,
     },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
+        event = { "BufReadPost", "BufNewFile" },
+        dependencies = {
+            { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+            { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+        },
+        opts = {
+            debug = true, -- Enable debugging
+            -- See Configuration section for rest
+        },
+        -- See Commands section for default commands if you want to lazy load on them
+    },
 }
