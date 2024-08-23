499c88663e7c3708cd8be0d09a6f32efa915ad51
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-08-23 07:57:46 +0700

    arrow: removed harpoon2 and changed to arrow

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -2,7 +2,7 @@ return {
     "otavioschwanck/arrow.nvim",
     opts = {
         show_icons = true,
-        leader_key = [[\]], -- Recommended to be a single key
+        leader_key = [[<cr>]], -- Recommended to be a single key
     },
     keys = {
         { "<cr>", desc = "Open Arrow bookmarks" },
--- lua/plugins/snipe.lua
+++ lua/plugins/snipe.lua
@@ -10,4 +10,5 @@ return {
         },
     },
     opts = {},
+    enabled = false,
 }
