e9f800c9882f802dfeb232dc656c319f9c3a1dd2
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-10-08 10:35:01 +0700

    cmp: experimental to blink.cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -5,6 +5,7 @@ return {
     },
     {
         "hrsh7th/nvim-cmp",
+        enabled = false,
         dependencies = {
             "hrsh7th/cmp-cmdline",
             "hrsh7th/cmp-nvim-lsp-document-symbol",
