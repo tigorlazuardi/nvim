a6ce0106a008b3396a6c3a1ac70b364461d51bd2
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-10-10 09:33:04 +0700

    oil: update keys and mappings

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,12 +4,38 @@ return {
         keymaps = {
             ["q"] = "actions.close",
             ["<bs>"] = "actions.parent",
+            ["h"] = "actions.parent",
+            ["l"] = "actions.select",
+            ["gv"] = {
+                callback = function()
+                    require("oil.actions").select.callback { vertical = true }
+                    require("oil.actions").close.callback()
+                end,
+                desc = "Open the entry in a vertical split and close the Oil window",
+                nowait = true,
+            },
+            ["gs"] = {
+                callback = function()
+                    require("oil.actions").select.callback { horizontal = true }
+                    require("oil.actions").close.callback()
+                end,
+                desc = "Open the entry in a horizontal split and close the Oil window",
+                nowait = true,
+            },
         },
     },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     cmd = { "Oil" },
     keys = {
-        { "-", "<cmd>Oil<cr>", desc = "Open Oil" },
+        {
+            "-",
+            function()
+                require("oil").open()
+                vim.defer_fn(function()
+                    require("oil").open_preview()
+                end, 150)
+            end,
+        },
     },
     enabled = true,
 }
