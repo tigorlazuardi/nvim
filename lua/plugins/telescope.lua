ed6114dc05abf4da2bdde4c8f13f98eb047c18ab
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-11-07 19:49:08 +0700

    telescope: disabled smart open

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,30 @@
 return {
-    {
-        "danielfalk/smart-open.nvim",
-        keys = {
-            {
-                "<leader><space>",
-                function()
-                    require("telescope").extensions.smart_open.smart_open { cwd_only = true }
-                end,
-                desc = "Open file or buffers",
-            },
-        },
-    },
-    {
-        "telescope.nvim",
-        opts = {
-            extensions = {
-                smart_open = {
-                    result_limit = 50,
-                },
-            },
-        },
-        keys = {
-            {
-                "<leader><space>",
-                false,
-            },
-        },
-    },
+    -- {
+    --     "danielfalk/smart-open.nvim",
+    --     keys = {
+    --         {
+    --             "<leader><space>",
+    --             function()
+    --                 require("telescope").extensions.smart_open.smart_open { cwd_only = true }
+    --             end,
+    --             desc = "Open file or buffers",
+    --         },
+    --     },
+    -- },
+    -- {
+    --     "telescope.nvim",
+    --     opts = {
+    --         extensions = {
+    --             smart_open = {
+    --                 result_limit = 50,
+    --             },
+    --         },
+    --     },
+    --     keys = {
+    --         {
+    --             "<leader><space>",
+    --             false,
+    --         },
+    --     },
+    -- },
 }
