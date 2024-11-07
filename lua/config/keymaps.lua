7cfd09db78fe0195edd3dd69b3f0c30a64a128c3
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-11-07 20:15:23 +0700

    lazygit: remove lazygit config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,10 +6,6 @@ require "config.neovide"
 
 vim.keymap.set("t", "<c-d>", "<C-\\><C-n>", { silent = true, desc = "Exit Terminal Mode" })
 
-if vim.fn.executable "lazygit" == 1 then
-    require "config.lazygit"
-end
-
 if vim.env.HYPRLAND_INSTANCE_SIGNATURE ~= nil and vim.fn.executable "footclient" == 1 then
     require "config.terminal"
 end
