61bd66f49ede37b338083b952c11c23d4f7b762d
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-12-25 20:55:51 +0700

    nvim-osc52: added clipboard config

--- lua/plugins/ssh.lua
+++ lua/plugins/ssh.lua
@@ -2,12 +2,25 @@ return {
     "ojroques/nvim-osc52",
     cond = vim.env.SSH_CLIENT ~= nil,
     config = function()
-        require("osc52").setup({})
+        require("osc52").setup {}
         vim.api.nvim_create_autocmd("TextYankPost", {
             callback = function()
                 require("osc52").copy(table.concat(vim.v.event.regcontents, "\n"))
             end,
             desc = "Copy to Clipboard from SSH Session",
         })
+        local function copy(lines, _)
+            require("osc52").copy(table.concat(lines, "\n"))
+        end
+
+        local function paste()
+            return { vim.fn.split(vim.fn.getreg "", "\n"), vim.fn.getregtype "" }
+        end
+
+        vim.g.clipboard = {
+            name = "osc52",
+            copy = { ["+"] = copy, ["*"] = copy },
+            paste = { ["+"] = paste, ["*"] = paste },
+        }
     end,
 }
