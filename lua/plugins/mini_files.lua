0e8b426890c7f5af2dba9f6fb93a6b276d0c5ec7
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-12-25 22:07:09 +0700

    mini_files: update deprecated code

--- lua/plugins/mini_files.lua
+++ lua/plugins/mini_files.lua
@@ -16,7 +16,7 @@ return {
                 local rhs = function()
                     -- Make new window and set it as target
                     local new_target_window
-                    vim.api.nvim_win_call(mf.get_target_window(), function()
+                    vim.api.nvim_win_call(mf.get_explorer_state().target_window, function()
                         vim.cmd(direction .. " split")
                         new_target_window = vim.api.nvim_get_current_win()
                     end)
