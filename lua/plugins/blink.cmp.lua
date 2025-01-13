23d57f4072348a0e90f1594f926b98f32bcf0257
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-01-13 21:48:24 +0700

    lazy: update

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -4,20 +4,7 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-        snippets = {
-            expand = function(snippet)
-                require("luasnip").lsp_expand(snippet)
-            end,
-            active = function(filter)
-                if filter and filter.direction then
-                    return require("luasnip").jumpable(filter.direction)
-                end
-                return require("luasnip").in_snippet()
-            end,
-            jump = function(direction)
-                require("luasnip").jump(direction)
-            end,
-        },
+        snippets = { preset = "luasnip" },
         completion = {
             documentation = {
                 window = {
@@ -75,14 +62,9 @@ return {
                     },
                 },
             },
-            list = {
-                selection = function(ctx)
-                    return ctx.mode == "cmdline" and "auto_insert" or "preselect"
-                end,
-            },
         },
         sources = {
-            default = { "lsp", "path", "luasnip", "buffer" },
+            default = { "lsp", "path", "snippets", "buffer" },
             cmdline = function()
                 local type = vim.fn.getcmdtype()
                 -- Search forward and backward
