defa56c27a050a519eb57c756b70572c8bd43451
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-01-15 15:39:36 +0700

    markdown: update

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,43 +1,8 @@
 return {
     {
-        "MeanderingProgrammer/markdown.nvim",
-        name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-        dependencies = { "nvim-treesitter/nvim-treesitter" },
-        config = function()
-            require("render-markdown").setup {}
-        end,
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+            render_modes = true,
+        },
     },
-    {
-        "brianhuster/live-preview.nvim",
-        cmd = { "LivePreview" },
-        ft = { "markdown" },
-        config = true,
-    },
-    -- {
-    --     "toppair/peek.nvim",
-    --     ft = { "markdown" },
-    --     cmd = { "Peek" },
-    --     build = "deno task --quiet build:fast",
-    --     opts = function()
-    --         vim.api.nvim_create_autocmd("FileType", {
-    --             desc = "Create peek command on markdown ft",
-    --             pattern = "markdown",
-    --             callback = function(ev)
-    --                 vim.api.nvim_buf_create_user_command(ev.buf, "Peek", function(ctx)
-    --                     local peek = require "peek"
-    --                     if peek.is_open() then
-    --                         if ctx.bang then
-    --                             peek.close()
-    --                         end
-    --                         return
-    --                     end
-    --                     peek.open()
-    --                 end, { bang = true })
-    --             end,
-    --         })
-    --         return {
-    --             app = { "chromium", "--new-window" },
-    --         }
-    --     end,
-    -- },
 }
