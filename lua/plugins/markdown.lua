db753d403fa7da1eb39e6cf30d6f549c459cd648
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-01-22 13:09:33 +0700

    markdown: update previewer

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -5,4 +5,12 @@ return {
             render_modes = true,
         },
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        enabled = false,
+    },
+    {
+        "davidgranstrom/nvim-markdown-preview",
+        ft = "markdown",
+    },
 }
