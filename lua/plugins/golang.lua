a366d0a2d2041edf659285ec4cecd21ea8c23031
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-02-03 14:38:33 +0700

    golang: disabled format manual

--- lua/plugins/golang.lua
+++ lua/plugins/golang.lua
@@ -39,60 +39,60 @@ return {
                     },
                 },
             },
-            setup = {
-                gopls = function(_, opts)
-                    require("lazyvim.util").lsp.on_attach(function(client, bufnr)
-                        if client.name == "gopls" then
-                            -- workaround for gopls not supporting semanticTokensProvider
-                            -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
-                            if not client.server_capabilities.semanticTokensProvider then
-                                local semantic = client.config.capabilities.textDocument.semanticTokens
-                                client.server_capabilities.semanticTokensProvider = {
-                                    full = true,
-                                    legend = {
-                                        tokenTypes = semantic.tokenTypes,
-                                        tokenModifiers = semantic.tokenModifiers,
-                                    },
-                                    range = true,
-                                }
-                            end
-                            -- end workaround
-
-                            -- run lsp imports code action on save.
-                            vim.api.nvim_create_autocmd("BufWritePre", {
-                                desc = "Auto format and organize imports on save (gopls)",
-                                group = vim.api.nvim_create_augroup("GoplsAutoFormat", {}),
-                                buffer = bufnr,
-                                callback = function(event)
-                                    local context = { source = { organizeImports = true } }
-                                    local params = vim.lsp.util.make_range_params()
-                                    params.context = context
-                                    local result =
-                                        vim.lsp.buf_request_sync(event.buf, "textDocument/codeAction", params, 3000)
-                                    if not result then
-                                        return
-                                    end
-                                    if not result[1] then
-                                        return
-                                    end
-                                    result = result[1].result
-                                    if not result then
-                                        return
-                                    end
-                                    if not result[1] then
-                                        return
-                                    end
-                                    local edit = result[1].edit
-                                    if not edit then
-                                        return
-                                    end
-                                    vim.lsp.util.apply_workspace_edit(edit, "utf-8")
-                                end,
-                            })
-                        end
-                    end)
-                end,
-            },
+            -- setup = {
+            --     gopls = function(_, opts)
+            --         require("lazyvim.util").lsp.on_attach(function(client, bufnr)
+            --             if client.name == "gopls" then
+            --                 -- workaround for gopls not supporting semanticTokensProvider
+            --                 -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+            --                 if not client.server_capabilities.semanticTokensProvider then
+            --                     local semantic = client.config.capabilities.textDocument.semanticTokens
+            --                     client.server_capabilities.semanticTokensProvider = {
+            --                         full = true,
+            --                         legend = {
+            --                             tokenTypes = semantic.tokenTypes,
+            --                             tokenModifiers = semantic.tokenModifiers,
+            --                         },
+            --                         range = true,
+            --                     }
+            --                 end
+            --                 -- end workaround
+            --
+            --                 -- run lsp imports code action on save.
+            --                 vim.api.nvim_create_autocmd("BufWritePre", {
+            --                     desc = "Auto format and organize imports on save (gopls)",
+            --                     group = vim.api.nvim_create_augroup("GoplsAutoFormat", {}),
+            --                     buffer = bufnr,
+            --                     callback = function(event)
+            --                         local context = { source = { organizeImports = true } }
+            --                         local params = vim.lsp.util.make_range_params()
+            --                         params.context = context
+            --                         local result =
+            --                             vim.lsp.buf_request_sync(event.buf, "textDocument/codeAction", params, 3000)
+            --                         if not result then
+            --                             return
+            --                         end
+            --                         if not result[1] then
+            --                             return
+            --                         end
+            --                         result = result[1].result
+            --                         if not result then
+            --                             return
+            --                         end
+            --                         if not result[1] then
+            --                             return
+            --                         end
+            --                         local edit = result[1].edit
+            --                         if not edit then
+            --                             return
+            --                         end
+            --                         vim.lsp.util.apply_workspace_edit(edit, "utf-8")
+            --                     end,
+            --                 })
+            --             end
+            --         end)
+            --     end,
+            -- },
         },
     },
     {
